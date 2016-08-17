ff9454953360ad034c9029cfd3361790b4aac9c3
Noah Wimmer <nwimmer123@yahoo.com>
2016-08-17 15:32:43 -0700

    more sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,8 @@
 class SessionsController < ApplicationController
   
-  def login
-    session[:user_id] = user.user_id
-    @current_user = user
-  end
-
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
-  end
-
-  def logged_in?
-    if current_user == nil
-      redirect_to new_session_path 
-    end
-  end
-
-  def logout
-    @current_user = session[:user_id] = nil
+  def new
+    @user = User.new
+    render :new
   end
 
 end
