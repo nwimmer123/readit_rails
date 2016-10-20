1c0971a455fea4e386eaf781d7dd61a024fb0042
Noah Wimmer <nwimmer123@yahoo.com>
2016-10-20 15:20:21 -0700

    nested commetns working independent of the rest of the app

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,34 @@
 class ReviewsController < ApplicationController
 
   def index
-    @reviews = Review.all
+    @user = current_user
+    @reviews = Review.hash_tree
   end
 
   def new
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
     @review = Review.new(parent_id: params[:parent_id]) 
 
-    redirect_to book_path(@book)
+    #redirect_to book_path(@book)
   end
 
   def create
-    @book = Book.find(params[:id])
-		@review = Review.create(review_params)
+    #@book = Book.find(params[:id])
+		if params[:review][:parent_id].to_i > 0
+	    parent = Review.find_by_id(params[:review].delete(:parent_id))
+	    @review = parent.children.build(review_params)
+	  else
+	    @review = Review.new(review_params)
+	  end
 
-		redirect_to book_path(@book)
+    if @review.save
+	    flash[:success] = 'Your comment was successfully added!'
+	    redirect_to reviews_path
+	  else
+	    render 'new'
+	  end
+
+		#redirect_to book_path(@book)
   end
 
   	private
