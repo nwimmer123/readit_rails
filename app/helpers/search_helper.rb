60e6a3126cdb9439c762125c9e6541a1652a114b
Noah Wimmer <nwimmer123@yahoo.com>
2016-11-05 16:13:36 -0700

    basic seach functions inserted

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,19 @@
 module SearchHelper
+
+  SECRET_KEY = Figaro.env.google_books
+
+  def book_url(book_name)
+    "https://www.googleapis.com/books/v1/volumes?q=#{book_name}&key=#{SECRET_KEY}"
+  end
+
+  def fetch_book_data(book_name)
+    uri = URI(book_url(book_name))
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)    
+  end
+
+  def display_results(book_name)
+    @book_data = fetch_book_data(book_name)
+  end 
+
 end
