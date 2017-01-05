6f2e84ba3a23ca66060abe8f39b6dbbdc24906bd
Noah Wimmer <nwimmer123@yahoo.com>
2017-01-05 15:23:17 -0800

    password reset working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,16 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :sendmail
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'mail.google.com',
+    user_name:            'readittogether10',
+    password:             'surfing69',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.raise_delivery_errors = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'mail.google.com',
+    user_name:            'readittogether10',
+    password:             'surfing69',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => "https://readittogether.herokuapp.com/" }
   config.action_mailer.raise_delivery_errors = true
+
 
 end
