cfec3d8c8bd6c50820d55604ed9ccc7f5c8a34c7
Noah Wimmer <nwimmer123@yahoo.com>
2017-04-18 17:31:20 -0700

    mailer notification

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,2 +1,9 @@
 class ReviewMailer < ApplicationMailer
+
+  def review_notification(review)
+    @review = review
+    @user = @review.user
+    mail(to: @user.email, subject: "Someone commented on your post!")
+
+  end
 end
